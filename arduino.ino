924cffbb59b1112b53f0b29e183c0623eae429a4
Jirayu Pharang <P_jirayu@kkumail.com>
2017-12-10 11:19:51 +0700

    Add arduino file

---
+++ arduino.ino
@@ -0,0 +1,25 @@
+const int buttonPin = 5; 
+int buttonState = LOW;
+
+void setup(){
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);  
+}
+
+void loop(){
+buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+ Serial.println(1, DEC); 
+    Serial.write(0x0A);
+    Serial.write(0x0D);
+    buttonState == LOW
+  } 
+  else {
+Serial.println(0, DEC); 
+    Serial.write(0x0A);
+    Serial.write(0x0D);
+  }
+} 
+   delay(1000);
+}
